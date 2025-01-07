342f0b0856e4b26b0626950035b641717b2c0946
alex55688 <N1116801@ntub.edu.tw>
2025-01-07 13:27:43 +0800

    刪掉不必要code

--- src/main/java/com/tsb/controller/intoController.java
+++ src/main/java/com/tsb/controller/intoController.java
@@ -10,25 +10,11 @@ import jakarta.servlet.http.HttpSession;
 import jakarta.websocket.Session;
 
 @Controller
-public class intoController {
+public class IntoController {
 
 	@RequestMapping(path = "/blog", method = {RequestMethod.GET})
 	public String index(Model model) {
 		model.addAttribute("valid", false);
 		return "index";
-	}
-	
-	@RequestMapping(path = "/login", method = {RequestMethod.GET, RequestMethod.POST})
-	public String login(Model model, HttpServletRequest request) {
-		HttpSession session =  request.getSession();
-		if("POST".equals(request.getMethod())) {
-			if(session.getAttribute("valid") != null && true == (boolean)session.getAttribute("valid")) {
-				System.out.println("帳號密碼正確");
-			}else {
-				model.addAttribute("validshow", true);
-				model.addAttribute("errormsg", "帳號密碼有誤");
-			}
-		}
-		return "login";
 	}
 }
